4163f06a8e8ee4a59dc2ac83faf7366c065d9ce6
nicolas.pedroso <nicolas.pda@hotmail.com>
2024-12-16 17:25:47 -0300

    fix:Charts Alinhados

---
+++ components/Contatos/ContatosTemporal.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="card">
+    <Chart
+      type="line"
+      :data="chartData"
+      :options="chartOptions"
+      class="h-[30rem]"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null,
+    };
+  },
+  mounted() {
+    this.chartData = this.setChartData();
+    this.chartOptions = this.setChartOptions();
+  },
+  methods: {
+    setChartData() {
+      const documentStyle = getComputedStyle(document.documentElement);
+
+      return {
+        labels: [
+          'Janeiro',
+          'Fevereiro',
+          'Março',
+          'Abril',
+          'Maio',
+          'Junho',
+          'Julho',
+        ],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--p-cyan-500'),
+            yAxisID: 'y',
+            tension: 0.4,
+            data: [65, 59, 80, 81, 56, 55, 10],
+          },
+          {
+            label: 'Dataset 2',
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--p-gray-500'),
+            yAxisID: 'y1',
+            tension: 0.4,
+            data: [28, 48, 40, 19, 86, 27, 90],
+          },
+        ],
+      };
+    },
+    setChartOptions() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--p-text-muted-color'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue(
+        '--p-content-border-color'
+      );
+
+      return {
+        stacked: false,
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y1: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              drawOnChartArea: false,
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    },
+  },
+};
+</script>
