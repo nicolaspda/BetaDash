e937e50cdd79b0b811eb1eaea5e282dbfa295829
nicolas.pedroso <nicolas.pda@hotmail.com>
2024-12-18 15:41:53 -0300

    Loading implementado

---
+++ pages/Loading.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div class="flex justify-center">
+      <img
+        src="https://i.postimg.cc/DyWW65wL/dina-logo.png"
+        width="200"
+        :class="logoClass"
+      />
+    </div>
+    <div class="w-80 mt-3">
+      <ProgressBar
+        :value="loading"
+        :showValue="false"
+        style="height: 7px"
+      ></ProgressBar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: 0,
+      interval: null,
+    };
+  },
+  mounted() {
+    this.startProgress();
+  },
+  methods: {
+    startProgress() {
+      this.interval = setInterval(() => {
+        let newValue = this.loading + 18;
+        if (newValue >= 100) {
+          newValue = 100;
+          this.endProgress();
+        }
+        this.loading = newValue;
+      }, 1400);
+    },
+    endProgress() {
+      clearInterval(this.interval);
+      this.interval = null;
+      setTimeout(() => {
+        this.$router.push('/Dash');
+      }, 1200);
+    },
+  },
+  computed: {
+    logoClass() {
+      return this.loading < 100
+        ? 'flex pb-3 mb-2 animate-fadein animate-duration-2000 animate-infinite'
+        : 'flex pb-3 mb-2 w-44 transition-all transition-duration-500 animate-delay-1000';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex justify-center items-center" style="height: 100vh">
+    <Card style="width: 460px" class="p-5">
+      <template #title>
+        <img
+          src="https://i.postimg.cc/DyWW65wL/dina-logo.png"
+          width="180"
+          class="flex mr-6 pb-3 mb-2"
+        />
+      </template>
+      <template #content>
+        <span class="block mb-5">Acesse sua conta:</span>
+        <div class="flex items-center gap-1 mb-3">
+          <label for="username" class="font-semibold w-24">Usuário</label>
+          <div :class="inputFocusEmail === true ? animation : 'h-8'"></div>
+          <InputText
+            id="username"
+            class="w-full"
+            autocomplete="off"
+            v-model="email"
+            @focus="changeFocus('email')"
+          />
+        </div>
+        <div class="flex items-center gap-1 mb-5">
+          <label for="password" class="font-semibold w-24"> Senha </label>
+          <div :class="inputFocusPass === true ? animation : 'h-8'"></div>
+          <Password
+            v-model="password"
+            fluid
+            :feedback="false"
+            @focus="changeFocus('password')"
+            toggleMask
+            class="w-full"
+          />
+        </div>
+      </template>
+      <template #footer>
+        <div class="flex justify-between items-center">
+          <div>
+            <a href="https://www.dinamize.com.br/" class="text-xs"
+              >Esqueci minha senha
+            </a>
+          </div>
+          <div class="flex justify-content-end gap-2">
+            <NuxtLink to="/Loading">
+              <Button
+                type="password"
+                label="Entrar"
+                @click="visible = false"
+              ></Button>
+            </NuxtLink>
+          </div>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animation:
+        'h-8 !w-1 bg-indigo-400 animate-scalein animation-duration-200 rounded-md',
+      inputFocusEmail: false,
+      inputFocusPass: false,
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    /*função que anima a tabulação dos inputs*/
+    changeFocus(clicked) {
+      if (clicked === 'email') {
+        this.inputFocusEmail = true;
+        this.inputFocusPass = false;
+      } else if (clicked === 'password') {
+        this.inputFocusEmail = false;
+        this.inputFocusPass = true;
+      }
+    },
+  },
+};
+</script>
