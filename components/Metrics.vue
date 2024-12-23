2d872df2e03c5f4434792f77ea00e1a633a17f83
nicolas.pedroso <nicolas.pda@hotmail.com>
2024-12-23 11:15:06 -0300

    Gráficos do gerador ajustados

---
+++ components/Metrics.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main">
+    <!--Controller Nav-->
+    <div
+      class="flex justify-between h-20 mx-11 mt-8 p-5 rounded-md shadow-sm bg-white text-2xl"
+    >
+      <div class="title flex items-center">
+        Analisar /
+        <span class="text-primary font-bold">&nbsp;Listas de Valores </span>
+      </div>
+    </div>
+    <div class="area flex mx-11">
+      <!--Conteúdo principal-->
+      <div class="content w-full mt-5">
+        <Card class="h-full rounded-md z-10">
+          <template #content>
+            <div class="unique py-8">
+              <p class="text-lg text-black">Campo único</p>
+              <div class="mt-6 flex justify-between">
+                <div class="graph flex-1"><MetricsPie /></div>
+                <div class="selector">Seletor</div>
+              </div>
+            </div>
+            <hr />
+            <div class="multiple py-8">
+              <p class="text-lg text-black">Campo múltiplo</p>
+              <div class="mt-6 flex justify-between">
+                <div class="graph flex-1"><MetricsBar /></div>
+                <div class="selector">Seletor</div>
+              </div>
+            </div>
+          </template>
+        </Card>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  data() {
+    return {
+      btnSelected: 'Sessões',
+    };
+  },
+  methods: {
+    DisplayContent(item) {
+      this.btnSelected = item.label;
+    },
+  },
+};
+</script>
+<style></style>
---
+++ components/Metrics/MetricsPie.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card flex justify-center">
+    <Chart
+      type="pie"
+      :data="chartData"
+      :options="chartOptions"
+      class="w-full md:w-[20rem]"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null,
+    };
+  },
+  mounted() {
+    this.chartData = this.setChartData();
+    this.chartOptions = this.setChartOptions();
+  },
+  methods: {
+    setChartData() {
+      const documentStyle = getComputedStyle(document.body);
+
+      return {
+        labels: ['A', 'B', 'C'],
+        datasets: [
+          {
+            data: [540, 325, 702],
+            backgroundColor: [
+              documentStyle.getPropertyValue('--p-cyan-500'),
+              documentStyle.getPropertyValue('--p-orange-500'),
+              documentStyle.getPropertyValue('--p-gray-500'),
+            ],
+            hoverBackgroundColor: [
+              documentStyle.getPropertyValue('--p-cyan-400'),
+              documentStyle.getPropertyValue('--p-orange-400'),
+              documentStyle.getPropertyValue('--p-gray-400'),
+            ],
+          },
+        ],
+      };
+    },
+    setChartOptions() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+
+      return {
+        plugins: {
+          legend: {
+            labels: {
+              usePointStyle: true,
+              color: textColor,
+            },
+          },
+        },
+      };
+    },
+  },
+};
+</script>
