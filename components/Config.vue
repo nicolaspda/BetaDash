d81bbde011297acd6767344df35e7d6ddf250e86
nicolas.pedroso <nicolas.pda@hotmail.com>
2024-12-24 11:51:54 -0300

    conteudo adicionado drawer

--- components/Config.vue
+++ components/Config.vue
@@ -10,12 +10,26 @@
           <span class="font-bold">Amy Elsner</span>
         </div>
       </template>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
+      <!--Código da Lista-->
+      <Fieldset>
+        <template #legend>
+          <div class="flex items-center pl-2">
+            <Avatar icon="pi pi-user" size="small" class="!bg-primary-100" />
+            <span class="font-bold p-2">Código da Lista</span>
+          </div>
+        </template>
+        <p class="m-0">Escolha a lista que deve refletir as informações:</p>
+      </Fieldset>
+      <!--Analytics-->
+      <Fieldset>
+        <template #legend>
+          <div class="flex items-center pl-2">
+            <Avatar icon="pi pi-google" size="small" class="!bg-orange-200" />
+            <span class="font-bold p-2">Google Analytics</span>
+          </div>
+        </template>
+        <p class="m-0">Configure sua conta do GA4:</p>
+      </Fieldset>
       <template #footer>
         <div class="flex items-center gap-2">
           <Button
