69c2bea04fff5f928145e325dcbebbf6ca3bcf1a
nicolaspda <nicolas.pda@hotmail.com>
2025-01-11 22:54:37 -0300

    MetricsPie added percentage

--- components/Metrics/MetricsPie.vue
+++ components/Metrics/MetricsPie.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="flex justify-center items-center">
-    <Dialog v-model:visible="visible" modal header="Edit Profile">
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Edit Profile"
+    >
       <template #header>
         <div class="inline-flex items-center justify-center gap-2">
-          <Avatar icon="pi pi-users" shape="circle" />
+          <Avatar
+            icon="pi pi-users"
+            shape="circle"
+          />
           <span class="font-bold whitespace-nowrap">Contatos relacionados</span>
         </div>
       </template>
@@ -59,7 +66,10 @@
       size="18rem"
       v-if="chartLoading"
     ></Skeleton>
-    <div v-if="selectedLvu == null" class="flex flex-col items-center">
+    <div
+      v-if="selectedLvu == null"
+      class="flex flex-col items-center"
+    >
       <p>Selecione um campo na lista</p>
       <Chart
         type="pie"
@@ -89,7 +99,7 @@
 
 <script>
 export default {
-  props: ['selectedLvu'],
+  props: ["selectedLvu"],
   data() {
     return {
       chartData: null,
@@ -109,66 +119,66 @@ export default {
       //Chamada para buscar o valor das Labels
       let getDataValue = [];
       const chamada = {
-        'contact-list_code': '7',
-        page_number: '1',
-        page_size: '10000',
+        "contact-list_code": "7",
+        page_number: "1",
+        page_size: "10000",
         search: [
           {
             field: this.selectedLvu.code_name,
-            operator: '!=',
-            value: '0',
+            operator: "!=",
+            value: "0",
           },
         ],
       };
 
       $fetch(
-        'https://proxy.cors.sh/https://api.dinamize.com/emkt/contact/search',
+        "https://proxy.cors.sh/https://api.dinamize.com/emkt/contact/search",
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Access-Control-Allow-Origin': '*',
-            'x-cors-api-key': 'temp_4be2c4562bb040588f036493d162b34f',
-            'Access-Control-Allow-Headers': 'x-requested-with',
-            Accept: 'application/json',
-            'auth-token': localStorage.getItem('auth_token'),
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+            "x-cors-api-key": "temp_4be2c4562bb040588f036493d162b34f",
+            "Access-Control-Allow-Headers": "x-requested-with",
+            Accept: "application/json",
+            "auth-token": localStorage.getItem("auth_token"),
           },
           body: chamada,
         }
       )
         .then((response) => {
-          if (response.code_detail === 'Sucesso') {
-            console.log('Sucesso em buscar valores dos campos e contatos');
+          if (response.code_detail === "Sucesso") {
+            console.log("Sucesso em buscar valores dos campos e contatos");
             console.log(response.body.items);
             getDataValue = response.body.items;
 
             //Chamada para buscar o nome das Labels
             let getDataName = [];
             const chamada = {
-              'contact-list_code': '7',
-              page_number: '1',
-              page_size: '10000',
+              "contact-list_code": "7",
+              page_number: "1",
+              page_size: "10000",
               field_code: this.selectedLvu.code,
             };
 
             $fetch(
-              'https://proxy.cors.sh/https://api.dinamize.com/emkt/field-lov/search',
+              "https://proxy.cors.sh/https://api.dinamize.com/emkt/field-lov/search",
               {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                  'Content-Type': 'application/json; charset=utf-8',
-                  'Access-Control-Allow-Origin': '*',
-                  'x-cors-api-key': 'temp_4be2c4562bb040588f036493d162b34f',
-                  'Access-Control-Allow-Headers': 'x-requested-with',
-                  Accept: 'application/json',
-                  'auth-token': localStorage.getItem('auth_token'),
+                  "Content-Type": "application/json; charset=utf-8",
+                  "Access-Control-Allow-Origin": "*",
+                  "x-cors-api-key": "temp_4be2c4562bb040588f036493d162b34f",
+                  "Access-Control-Allow-Headers": "x-requested-with",
+                  Accept: "application/json",
+                  "auth-token": localStorage.getItem("auth_token"),
                 },
                 body: chamada,
               }
             )
               .then((response2) => {
-                if (response2.code_detail === 'Sucesso') {
-                  console.log('Sucesso em buscar Labels dos campos');
+                if (response2.code_detail === "Sucesso") {
+                  console.log("Sucesso em buscar Labels dos campos");
                   console.log(response2.body.list);
                   getDataName = response2.body.list;
 
@@ -176,7 +186,7 @@ export default {
                   const filterValues = getDataValue.map(
                     (item) => item.custom_fields[this.selectedLvu.code_name]
                   );
-                  console.log('filterVALEUS');
+                  console.log("filterVALEUS");
                   console.log(filterValues);
 
                   const acum = filterValues.reduce((acc, item) => {
@@ -192,7 +202,7 @@ export default {
                     );
                     return values ? values.value : null;
                   });
-                  console.log('Label found:');
+                  console.log("Label found:");
                   console.log(mergeLabel);
 
                   // Filtra os emails para mostrar no gráfico comparando os valores de campo (numeros disponíveis) com o item LVU de todos os contatos
@@ -211,12 +221,12 @@ export default {
                     }, [])
                   );
 
-                  console.log('Emails found:');
+                  console.log("Emails found:");
                   console.log(findContact);
 
                   // Filtra os valores acumulados
                   const filterSets = Object.values(acum);
-                  console.log('Qtd de valores acumulada:');
+                  console.log("Qtd de valores acumulada:");
                   console.log(filterSets);
                   // Adiciona as informações no gráfico
                   this.chartData = {
@@ -240,34 +250,34 @@ export default {
                   this.chartLoading = false;
                 } else {
                   this.$toast.add({
-                    severity: 'error',
-                    summary: 'Atenção',
+                    severity: "error",
+                    summary: "Atenção",
                     detail: response.code_detail,
                     life: 3000,
                   });
-                  console.log('Falha');
+                  console.log("Falha");
                 }
               })
               .catch((error) => {
-                console.error('Buscar dados falhou:', error);
+                console.error("Buscar dados falhou:", error);
               });
           } else {
             this.$toast.add({
-              severity: 'error',
-              summary: 'Atenção',
+              severity: "error",
+              summary: "Atenção",
               detail: response.code_detail,
               life: 3000,
             });
-            console.log('Falha');
+            console.log("Falha");
           }
         })
         .catch((error) => {
-          console.error('Buscar dados falhou:', error);
+          console.error("Buscar dados falhou:", error);
         });
     },
     setChartOptions() {
       const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColor = documentStyle.getPropertyValue("--p-text-color");
 
       return {
         plugins: {
@@ -275,6 +285,20 @@ export default {
             labels: {
               usePointStyle: true,
               color: textColor,
+            },
+          },
+          tooltip: {
+            callbacks: {
+              label: function (tooltipItem) {
+                const dataset = tooltipItem.dataset;
+                const total = dataset.data.reduce(
+                  (acc, value) => acc + value,
+                  0
+                );
+                const currentValue = dataset.data[tooltipItem.dataIndex];
+                const percentage = ((currentValue / total) * 100).toFixed(2); // Calcula a porcentagem
+                return `${currentValue} (${percentage}%)`; // Exibe o valor e a porcentagem
+              },
             },
           },
         },
@@ -293,10 +317,10 @@ export default {
           const canvas = event.native.target;
           if (elements.length) {
             // Cursor "pointer" quando está sobre uma fatia
-            canvas.style.cursor = 'pointer';
+            canvas.style.cursor = "pointer";
           } else {
             // Cursor padrão quando não está sobre uma fatia
-            canvas.style.cursor = 'default';
+            canvas.style.cursor = "default";
           }
         },
       };
