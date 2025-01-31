50a44acab942316492b990220a47830e45f1c56f
nicolas.pedroso <nicolas.pda@hotmail.com>
2025-01-31 11:20:08 -0300

    add: Funnel updateValues implementado

--- components/Sessoes/SessoesFunnel.vue
+++ components/Sessoes/SessoesFunnel.vue
@@ -1,34 +1,50 @@
 <template>
-  <div class="funnel animate-fadein animate-duration-400"></div>
+  <div>
+    <div class="funnel animate-fadein animate-duration-400"></div>
+    <button @click="updateGraph" class="btn">Atualizar Gráfico</button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      graph: null,
+      currentValues: [
+        [2000, 4000, 6000],
+        [3000, 1000, 1700],
+        [200, 30, 130],
+      ],
+    };
   },
   mounted() {
     this.loadGraph();
   },
   methods: {
     loadGraph() {
-      var graph = new FunnelGraph({
+      document.querySelector('.funnel').innerHTML = '';
+      this.graph = new FunnelGraph({
         container: '.funnel',
         gradientDirection: 'horizontal',
         data: {
           labels: ['Impressions', 'Add To Cart', 'Purchase'],
           subLabels: ['Direct', 'Social Media', 'Ads'],
           colors: [['#FFB178', '#FF78B1', '#FF3C8E'], '#6366f1', ['#363885']],
-          values: [
-            [2000, 4000, 6000],
-            [3000, 1000, 1700],
-            [200, 30, 130],
-          ],
+          values: this.currentValues,
         },
         displayPercent: true,
         direction: 'horizontal',
       });
-      graph.draw();
+      this.graph.draw();
+    },
+
+    updateGraph() {
+      this.currentValues = [
+        [8000, 4500, 2000],
+        [3200, 1200, 1900],
+        [250, 50, 150],
+      ];
+      this.loadGraph();
     },
   },
 };
@@ -39,11 +55,23 @@ export default {
   width: 100%;
   height: 200px;
 }
-.svg-funnel-js__labels .label__value,
-.svg-funnel-js__subLabel--title {
-  color: #111324 !important; /* Altere para a cor desejada */
+.btn {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #6366f1;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.btn:hover {
+  background-color: #4f46e5;
 }
 
+.svg-funnel-js__labels .label__value,
+.svg-funnel-js__subLabel--title {
+  color: #111324 !important;
+}
 .svg-funnel-js__labels .label__title {
   color: #6366f1 !important;
 }
